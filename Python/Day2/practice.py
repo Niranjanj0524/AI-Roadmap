1d1d64c1980766dc28be4e854ee207c3658d2508
Niranjanj0524 <niranjanjagtap0524@gmail.com>
2026-08-06 15:14:25 +0530

    Day 2: Variables, Data Types, Type Conversion, Input & Output

---
+++ Python/Day2/practice.py
@@ -0,0 +1,38 @@
+print("Name: Niranjan")
+print("Age: 22 years")
+
+college_name = "RMDSTIC"
+print("City: Solapur, State: MH")
+
+var1 = "Python"
+var2 = "is"
+var3 = "easy."
+print(var1, var2, var3)
+
+print(type(45))
+print(type(3.14))
+print(type("Hello"))
+
+num_str = "100"
+num_int = int(num_str)
+print("Converted integer:", num_int)
+
+age_int = 25
+age_str = str(age_int)
+print("Converted string:", age_str)
+
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+addition = num1 + num2
+print("Sum of two numbers:", addition)
+
+n1 = float(input("Enter first number for average: "))
+n2 = float(input("Enter second number for average: "))
+n3 = float(input("Enter third number for average: "))
+average = (n1 + n2 + n3) / 3
+print("Average of three numbers:", average)
+
+print(f"Hello {user_name}! Your age is {user_age} and your sum is {addition}.")
