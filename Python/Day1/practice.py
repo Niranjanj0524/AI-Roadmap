482b2a0b86b5deb1cdc346268a22129930504caf
Niranjanj0524 <niranjanjagtap0524@gmail.com>
2026-08-06 12:38:59 +0530

    Day 1: Python Introduction and First Program

---
+++ Python/Day1/practice.py
@@ -0,0 +1,13 @@
+print("Welcome")
+print("to")
+print("Python")
+
+print("My name is Niranjan")
+
+print("I am learning AI")
+
+print("Python is fun")
+
+print("Welcome")
+print("to")
+print("Artificial Intelligence")
