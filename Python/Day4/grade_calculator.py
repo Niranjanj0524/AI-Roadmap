e7842117671083dc9567f245431d774da01760bc
Niranjanj0524 <niranjanjagtap0524@gmail.com>
2026-08-07 07:04:38 +0530

    Day 4: Conditional Statements and Grade Calculator

---
+++ Python/Day4/grade_calculator.py
@@ -0,0 +1,30 @@
+#Greater than Two number
+# num1 = int(input("Enter Number1 : "))
+# num2 = int(input("Enter Number2 : "))
+
+# if(num1 > num2):
+#     print(f"{num1} is greather than {num2}")
+# else:
+#     print(f"{num2} is greather than {num1}")
+
+#FInd odd or even
+# num = int(input("Enter Number : "))
+
+# if(num % 2 == 0):
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+#Grade Calculator
+marks = int(input("Enter marks : "))
+
+if marks >= 90 and marks <= 100:
+    print("Grade A")
+elif marks >= 75 and marks <= 89:
+    print("Grade B")
+elif marks >= 50 and marks <= 74:
+    print("Grade C")
+elif marks >= 35 and marks <= 49:
+    print("Grade D")
+else:
+    print("Fail")
