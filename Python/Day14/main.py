de744a9adb8129cc8fca913cc3235037a4407bae
Niranjanj0524 <niranjanjagtap0524@gmail.com>
2026-08-15 10:08:50 +0530

    Day 14: Modules Packages pip and Virtual Environment

---
+++ Python/Day14/main.py
@@ -0,0 +1,11 @@
+import calculator
+from mypackage.operations import square, cube
+
+
+print("Addition: ", calculator.add(10,5))
+print("Substraction: ", calculator.sub(10,5))
+print("Multiplication: ", calculator.mul(10,5))
+print("Division: ", calculator.div(10,5))
+
+print("Square: ", square(5))
+print("cube: ", cube(5))
